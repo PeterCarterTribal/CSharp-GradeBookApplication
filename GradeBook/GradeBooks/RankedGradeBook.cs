2cd5ddf77643d6881ea267838063042e717d56f5
Peter Carter <Peter.Carter@tribalgroup.com>
2018-02-19 11:50:54 +0000

    add letter grade override method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,26 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numStudents = Students.Count;
+
+            if (numStudents < 5)
+                throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work");
+
+            int twentyPercentInterval = (int)(numStudents * 0.2);
+
+            var betterStudents = Students.Count(s => s.AverageGrade > averageGrade);
+
+            int i = 1;
+            char[] passLetters = new char[] {'A', 'B', 'C', 'D', 'F'};
+
+            var index = betterStudents / twentyPercentInterval;
+
+            return passLetters[index];
+
         }
     }
 }
